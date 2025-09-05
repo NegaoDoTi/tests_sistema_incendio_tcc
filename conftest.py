589eac6cc6dfca560277405fad53b76127099601
Tafnes Diogo Torres <tafnes.diogo@gmail.com>
2025-09-04 23:51:18 -0300

    fix: alterando as docstrings

--- conftest.py
+++ conftest.py
@@ -4,28 +4,29 @@ from utils.waits import Waits
 
 @pytest.fixture(scope="function")
 def driver():
+    """Função que cria o driver para os testes do pytest
+
+    Yields:
+        WebDriver: Driver chrome do selenium
     """
-    Esta fixture cria uma instância do driver antes de cada teste
-    e a destrói (fecha o navegador) ao final.
-    """
-    # --- SETUP (Executa antes do teste) ---
     driver_instance = Driver().get_chrome_driver()
 
     if isinstance(driver_instance, dict):
         pytest.fail(f"Erro ao iniciar o driver: {driver_instance.get('exception')}")
 
-    # A palavra 'yield' entrega o driver para o teste
     yield driver_instance
 
-    # --- TEARDOWN (Executa depois do teste) ---
-    print("\nFechando o navegador...")
     driver_instance.quit()
 
 
 @pytest.fixture(scope="function")
 def waits(driver):
-    """
-    Fixture que cria uma instância da sua classe Waits,
-    já recebendo o driver da fixture anterior.
+    """Função que cria classe de Waits para os tests do Pytest
+
+    Args:
+        driver (WebDriver): Driver chrome do selenium
+
+    Returns:
+        Waits: classe de waits para selenium
     """
     return Waits(driver)
